1f3ad33ba7e3f5394f7d037709ef9319113b4045
Dan Morse <morsedan@outlook.com>
2019-06-01 10:00:34 -0700

    added model

---
+++ Sprint3Challenge-Pokedex/Models/Pokemon.swift
@@ -0,0 +1,37 @@
+//
+//  Pokemon.swift
+//  Sprint3Challenge-Pokedex
+//
+//  Created by morse on 6/1/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable, Hashable {
+    let name: String
+    let id: Int
+    let abilities: [Ability]
+    let types: [TypeObject]
+    let sprites: [Sprite]
+}
+
+struct Ability: Codable, Hashable {
+    let ability: [String: AbilityCharacteristic]
+}
+
+struct AbilityCharacteristic: Codable, Hashable {
+    let name: String
+}
+
+struct TypeObject: Codable, Hashable {
+    let type: [String: [TypeCharacteristic]]
+}
+
+struct TypeCharacteristic: Codable, Hashable {
+    let name: String
+}
+
+struct Sprite: Codable, Hashable {
+    let frontShiney: URL
+}
